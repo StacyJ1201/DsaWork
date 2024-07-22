ad421f9d3a4d4a8140cf51d2748aba1de7ae7cb2
StacyJ1201 <stacyjones12335@gmail.com>
2024-07-22 10:58:54 -0400

    More Shell Sort added

--- src/SortingAlgos/ShellSort.java
+++ src/SortingAlgos/ShellSort.java
@@ -6,7 +6,7 @@ public class ShellSort {
     public static void main(String[] args) {
         int[] arr = {12, 34, 54, 2, 3, 19, 36, 45, 117};
 
-        System.out.println(Arrays.toString(shellSort(arr)));
+        System.out.println(Arrays.toString(shellSortDivided(arr)));
 
     }
     public static int[] shellSort(int[] array) {
@@ -21,6 +21,22 @@ public class ShellSort {
                         array[j] = array[j - incr];
                     }
                     array[j] = temp;
+                }
+            }
+        }
+        return array;
+    }
+
+    private static int[] shellSortDivided(int[] array){
+
+        for (int gap = array.length/2; gap > 0; gap/=2) {
+            for (int i = gap; i < array.length; i++) {
+                int j = i;
+                while (j >= gap && array[j] <= array[j - gap]){
+                    int temp = array[j];
+                    array[j] = array[j-gap];
+                    array[j-gap] = temp;
+                    j-=gap;
                 }
             }
         }
